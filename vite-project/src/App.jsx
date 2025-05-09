eb2d7294329bdd5f2e0418e9a10b1ab8a5ef611c
Soham Patil <145631825+Soham271@users.noreply.github.com>
2025-05-09 21:41:13 +0530

    Add files via upload

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
+
+import DashBoard from "./pages/Dashboard"; // Correctly import the component
+import FileUpload from "./pages/Hello";
+import SignUp from "./pages/SignUp";
+import PDFUploader from "./pages/Hello";
+import HI from "./pages/HI";
+import { Velustro } from "uvcanvas";
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+
+    {
+      path: "/dashboard",
+      element: <DashBoard></DashBoard>,
+    },
+    {
+      path: "/sign",
+      element: <SignUp />,
+    },
+    {
+      path: "/check",
+      element: <HI />,
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
+
+export default App;
